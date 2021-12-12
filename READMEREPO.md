2ab45849260ee2e22300a03a71e6ee7c94933516
LiamCurmideGray <liam.curmidegray.17@um.edu.mt>
2021-12-12 23:08:24 +0100

    Initial ReadMe files

--- READMEREPO.md
+++
@@ -1,2 +0,0 @@
-# Mobile-Single-Sign-On
-This Repository is to be used for the lecture demonstration for the Single-Sign-On
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Form, Alert } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+import { Form, Alert, Button } from "react-bootstrap";
 import { useUserAuth } from "../context/UserAuthContext";
 
 const Signup = () => {
@@ -16,7 +15,7 @@ const Signup = () => {
     setError("");
     try {
       await signUp(email, password);
-      navigate("/");
+      navigate("/home");
     } catch (err) {
       setError(err.message);
     }
