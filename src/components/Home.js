92c36df7198f40ef9f0f62748ca19b44c6a29b58
LiamCurmideGray <liam.curmidegray.17@um.edu.mt>
2021-12-13 03:28:51 +0100

    small fix Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,21 +4,21 @@ import { Button } from "react-bootstrap";
 
 
 const Home = () => {
-// From ProtectedRoute.md Section - Adding Code, do step 1 
+// From Home.md Section - Adding Code, do step 1 
 
-// From ProtectedRoute.md Section - Adding Code, do step 2 
+// From Home.md Section - Adding Code, do step 2 
 
-// From ProtectedRoute.md Section - Adding Code, do step 5 
+// From Home.md Section - Adding Code, do step 5 
  
   return (
     <>
       <div className="p-4 box mt-3 text-center">
         Hello Welcome <br />
-        {/*From ProtectedRoute.md Section - Adding Code, do step 3 */}
+        {/*From Home.md Section - Adding Code, do step 3 */}
 
       </div>
       <div className="d-grid gap-2">
-        {/*From ProtectedRoute.md Section - Adding Code, do step 4 */}
+        {/*From Home.md Section - Adding Code, do step 4 */}
         <Button  variant="primary">
 	       Log out
        </Button>
