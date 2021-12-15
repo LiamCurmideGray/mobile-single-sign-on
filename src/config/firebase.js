671742b87016a1e8d287dda8e62222a9c261df81
LiamCurmideGray <liam.curmidegray.17@um.edu.mt>
2021-12-15 02:09:18 +0100

    default firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import { initializeApp } from "firebase/app";
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
+const  firebaseConfig = {
+  apiKey:  "YourApiKey"
+  authDomain: "YourAuthDomain",
+  projectId:  "YourProjectId",
+  storageBucket:  "YourStorageBucket",
+  messagingSenderId:  "YourMessagingSenderId",
+  appId:  "YourAppId",
+  };
 
 // Initialize React Application
 const app = initializeApp(firebaseConfig);
